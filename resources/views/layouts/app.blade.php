65ee4e0658504872e7dba423965e2ef7b9e37b37
Richard Tonata <rtonata@gmail.com>
2022-07-06 12:20:39 +0200

    Added Enrolment adjustments on the menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -89,6 +89,13 @@
           </svg> Enrolment</a>
       </li>
       @endpermission
+      @permission('enrolment')
+      <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="/enrolment">
+          <svg class="c-sidebar-nav-icon">
+            <use xlink:href="{{asset('new/node_modules/@coreui/icons/sprites/free.svg#cil-pencil')}}"></use>
+          </svg> Enrolment Adjustments</a>
+      </li>
+      @endpermission
       @permission('cancel-enrolment')
       <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="/cancel-registration">
           <svg class="c-sidebar-nav-icon">
