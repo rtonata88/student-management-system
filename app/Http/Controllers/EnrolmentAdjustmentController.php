8e1325ee1b8475ec528277ba991ec2bb526f3a1a
Richard Tonata <rtonata@gmail.com>
2022-07-06 12:30:53 +0200

    Added method to filter students in the EnrolmentAdjustmentController file

--- app/Http/Controllers/EnrolmentAdjustmentController.php
+++ app/Http/Controllers/EnrolmentAdjustmentController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Student;
 use Illuminate\Http\Request;
+
+use Session;
 
 class EnrolmentAdjustmentController extends Controller
 {
@@ -14,5 +17,34 @@ class EnrolmentAdjustmentController extends Controller
     public function index()
     {
         return view('Management.Enrolment-Adjustment.Search');
+    }
+
+    public function filter(Request $request)
+    {
+
+        if (isset($request->student_number)) {
+            $student = Student::where('student_number2', $request->student_number)->first();
+            if ($student) {
+                return redirect()->route('enrolment.adjustment.showScreen', $student->id);
+            }
+        }
+
+        if (isset($request->names)) {
+            $students = Student::where('surname', 'like', '%' . $request->names . '%')
+                ->orwhere('student_names', 'like', '%' . $request->names . '%')
+                ->get();
+
+            if (count($students)) {
+                if (count($students) === 1) {
+                    return redirect()->route('enrolment.adjustment.showScreen', $students->first()->id);
+                } else {
+                    return view('Management.Enrolment-Adjustment.Search', compact('students'));
+                }
+            }
+        }
+
+        Session::flash('not_found', 'The entered student number does not match any record. Please make sure you have entered a correct student number');
+
+        return view('Management.Enrolment-Adjustment.Search', compact('students'));
     }
 }
