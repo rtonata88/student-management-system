4be9786b2247cc517bab860ec4fbe32bc9a90895
Givenazo <givenveiko@gmail.com>
2025-09-07 14:32:18 +0200

    Added the create marks migrations

--- database/migrations/2025_09_03_120000_create_marks_suppressions_table_new.php
+++ database/migrations/2025_09_03_120000_create_marks_suppressions_table_new.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMarksSuppressionTableNew extends Migration
+class CreateMarksSuppressionTableFixed extends Migration
 {
     /**
      * Run the migrations.
